b7a093609a4b933a52a49945a847746b18033a67
Kevin He <kevin.he@salesforce.com>
2023-11-06 21:49:21 -0800

    send request and show loading

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Client } from "@notionhq/client";
 import { Text } from "ink";
+import Spinner from "ink-spinner";
 
 type Props = {
-  name: string | undefined;
+  token: string | undefined;
 };
 
-export default function App({ name = "Stranger" }: Props) {
+export default function App({ token }: Props) {
+  const [steps, setStep] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const notion = new Client({ auth: token });
+    const fetchScripts = async () => {
+      const response = await notion.search({
+        query: "notion-sh",
+        filter: {
+          // you can either search "page" or "database"
+          // here we assume that user have created a root page called "notion-sh"
+          value: "page",
+          property: "object",
+        },
+      });
+
+      if (response.results.length === 0) {
+        setIsLoading(false);
+        setIsLoaded(true);
+        return;
+      }
+
+      setIsLoading(false);
+      setIsLoaded(true);
+    };
+    fetchScripts();
+  }, []);
+
   return (
     <Text>
-      Hello, <Text color="green">{name}</Text>
+      {isLoading && <Spinner type="dots" />}
+      {isLoaded && <Text color="green">✅</Text>}
+      <Text color="green">Loading scripts from notion database...</Text>
     </Text>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 import React from "react";
 import { render } from "ink";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import App from "./app.jsx";
 
-render(<App name={undefined} />);
+const argv = yargs(hideBin(process.argv)).options({
+  t: {
+    alias: "token",
+    demandOption: "Please provide a token via --token or -t",
+    describe: "Auth token",
+    type: "string",
+  },
+}).argv;
+
+// @ts-ignore
+const { token } = argv;
+
+render(<App token={token} />);
